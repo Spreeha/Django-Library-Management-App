295e4408ec815c7e6e9eaeb02cfddb794ae32411
Spreeha Dutta <spreehadutta@gmail.com>
2022-09-29 17:57:41 +0530

    Update admin.py

--- MyLib/admin.py
+++ MyLib/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+admin.site.register(Buyers)
+admin.site.register(Sellers)
+admin.site.register(Books)
